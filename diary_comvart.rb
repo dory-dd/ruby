686118e5ab81d738200d26472d8173e72720f0c1
小林 秀樹 <hitokui@MacBook-Pro.local>
2016-09-07 08:16:47 +0900

    暗中模索

--- diary_comvart.rb
+++ diary_comvart.rb
@@ -1,8 +1,16 @@
 	require 'rexml/document'
 	def read_xml
 		doc = REXML::Document.new(open("/Users/hitokui/Lab/mixi_export/adiary.xml"))
-		puts doc.elements["diary/day"].attributes['date']
-		puts doc.elements["diary/day"].attributes['title']
-		puts doc.elements["diary/day/body"].text
+
+		d = doc.elements["diary/day"].to_a
+		d.each do |day|
+			#title = doc.elements["diary/day"].attributes['title'].to_s
+			#doc.elements.each("diary/day/body") do |body|
+			#body.to_s
+			#c = c + body.length
+			#puts c
+		puts day
+		end
+		#puts body.length
 	end
-	read_xml
+read_xml
