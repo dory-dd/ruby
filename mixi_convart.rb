6ed454475cc81b8f19265259adc8883cbe3f4f61
小林 秀樹 <hitokui@MacBook-Pro.local>
2016-09-15 00:12:25 +0900

    もうすぐ

--- mixi_convart.rb
+++ mixi_convart.rb
@@ -1,19 +1,15 @@
 class Mixi_to_markdown
 	require 'rexml/document'
-	mixi_xml = "/Users/hitokui/Lab/mixi_export/adiary.xml"
-	post_path = "/Users/hitokui/Lab/dory-dd.github.io/_posts/MarkdownWriteTest.markdown"
-
-	def initialize(filename)
-	#いろいろ
-	end
+	Mixi_xml = "/Users/hitokui/Lab/mixi_export/adiary.xml"
+	Post_path = "/Users/hitokui/Lab/dory-dd.github.io/_posts/"
 
 	def make_filenames(date,title)
-		filename = 	date + "-"
-		filename = +title
-		filename = +"\.markdown"
+		filename =  +date + "-"
+		filename += +title
+		filename += +".markdown"
  	end
 
-	def make_header(title,date,category = "mixi",body)
+	def make_header(title,date,category = "mixi")
 	   header =  "---" + "\n"
 		 header += +"layout: 		post" + "\n"
 		 header += +"title:  		\"#{title}\"" + "\n"
@@ -28,22 +24,28 @@ class Mixi_to_markdown
 		bodies = body
 	end
 
-	def read_xml(mixi_xml)
-		doc = REXML::Document.new(open(mixi_xml))
+	def read_xml
+		bodies = Hash.new
+		doc = REXML::Document.new(open(Mixi_xml))
 		doc.elements.each("/diary/day") do |day|
-			title = day.attributes['title']
-			date = day.attributes['date']
-			body = day.elements['body'].text
+			bodies["title"] = day.attributes['title']
+			bodies["date"] = day.attributes['date']
+			bodies["body"] = day.elements['body'].text
+			#test
+			#return make_filenames(date,title)
+			return bodies
 		end
 	end
 #read_xml
 
 	def make_markdown
-			File.write(post_path,bodies)
+			file_path = Post_path + make_filenames(date,title)
+			write_body = make_header(title,date) + body
+			File.write(file_path,write_body)
 	end
-
-puts read_xml
 
 end
 
-Mixi_to_markdown.new("")
+mixi = Mixi_to_markdown.new
+#title = mixi.read_xml
+mixi.make_markdown
